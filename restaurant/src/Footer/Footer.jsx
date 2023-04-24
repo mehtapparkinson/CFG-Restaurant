76be1d4294f425cc2a1c0f43b2e61e713c905310
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2023-04-24 15:56:25 +0200

    Footer rendered and styled

---
+++ restaurant/src/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./footer.css"
+
+const Footer = () => {
+  return (
+   <footer>
+     <div className="footer-logos">
+      <img src="https://cdn4.iconfinder.com/data/icons/social-media-icons-the-circle-set/48/facebook_circle-512.png" alt="Facebook" />
+      <img src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-instagram-new-circle-512.png" alt="Instagram" />
+      <img src="https://cdn4.iconfinder.com/data/icons/social-media-icons-the-circle-set/48/twitter_circle-512.png" alt="Twitter" />
+    </div>
+   </footer>
+  )
+}
+
+export default Footer
