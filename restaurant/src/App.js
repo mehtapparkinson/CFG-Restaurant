1bc01e8eac5b1b80827ed6a4340e9a7a1e8dad32
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2023-04-24 14:01:51 +0200

    navbar elements created, styled

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
      
     </div>
   );
---
+++ restaurant/src/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./navbar.css";
+import logo from './CFGlogo.svg'
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li className="nav-left">
+            <img src={logo} alt="Logo" className="logo"/>
+            <a className="logotext">Dolce Bistro</a>
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Menu</a>
+        </li>
+        <li>
+          <a href="#">Contact Us</a>
+        </li>
+        <li>
+          <a href="#">Reservations</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
