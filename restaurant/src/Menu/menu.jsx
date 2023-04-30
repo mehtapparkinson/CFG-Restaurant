55a3df0e632c1c39bde94ed21ec62d4cc58f5e8a
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2023-04-30 21:24:01 +0300

    menu css updates

--- restaurant/src/Menu/menu.jsx
+++ restaurant/src/Menu/menu.jsx
@@ -3,15 +3,17 @@ import "./menu.css";
 
 const Menu = () => {
   return (
-    <div className=".menu-container ">
-      <img src="pizza.jpg" className="photo1" />
-      <img src="spaghetti.jpg" className="photo2" />
+    <div className="menu-container ">
+      <img src="https://i.ibb.co/x6FyDBD/10.png" className="menu-dishes" />
+      <div className="text-container">
       <h2 className="text">LOREM IPSUM</h2>
       <p className="text">
         Lorem Ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices
         leo tellus, vel condimentum felis efficitur eget.
       </p>
       <button>Book Now</button>
+      </div>
+     
     </div>
   );
 };
