e77bc23142d73b264244878ec99800c914da897c
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2023-05-02 15:44:25 +0300

    buttons scroll effect implemented and minor css changes

--- restaurant/src/Hero/Hero.jsx
+++ restaurant/src/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-scroll'
 import "./hero.css"
 
 const Hero = () => {
@@ -6,7 +7,9 @@ const Hero = () => {
     <div className='hero-section' id="home">
         <h2>DOLCE BISTRO</h2>
         <h1>A TASTE OF ITALY</h1>
-        <button>Dine with us</button>
+        <button className='hero-button'>
+        <Link to="reservations" smooth={true} duration={500}>Dine with us</Link>
+        </button>
 
     </div>
   )
